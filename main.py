401f6d5f6d2bd66a18730fb175a4534d0a297989
simonguenther <simonpublic@fam-guenther.com>
2020-03-14 19:15:58 +0100

    todo added

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from crawler_avanti import Avanti_Crawler
 from crawler_oemv import OEMV_Crawler
 
+"""
+    TODO
+    - implement central logging
+    - use proxy for init session at avanti and ömv
+    
+"""
+
 avanti = Avanti_Crawler()
 oemv = OEMV_Crawler()
 
-avanti.crawl()
-oemv.crawl()
+#avanti.crawl()
+#oemv.crawl()
